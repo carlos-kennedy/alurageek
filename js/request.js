24d636a745c0094e97f0e9617c764a998789978d
carlos-kennedy <carlosjustcarlos@outlook.com>
2024-11-10 19:56:05 -0300

    :sparkles: feat: Adding get and post requests so that images, names and values ​​are dynamically exchanged

---
+++ js/request.js
@@ -0,0 +1,48 @@
+// requests.js
+export async function fetchProdutos() {
+  try {
+    const response = await fetch("http://localhost:3000/produtos");
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    const produtos = await response.json();
+    return produtos;
+  } catch (error) {
+    console.error("Erro ao buscar produtos:", error);
+    return null;
+  }
+}
+
+export async function createProduct(product) {
+  try {
+    const response = await fetch("http://localhost:3000/produtos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    const newProduct = await response.json();
+    return newProduct;
+  } catch (error) {
+    console.error("Erro ao criar o produto", error);
+    return null;
+  }
+}
+
+export async function deleteProduct(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/produtos/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    console.log(`Produto com id ${id} excluído com sucesso !`);
+  } catch (error) {
+    console.error("Erro ao excluir produto:", error);
+  }
+}
